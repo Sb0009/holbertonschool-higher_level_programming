520c8a2b5683feac90b3761883274c2d1d92d1a9
siham <phenissia2009@hotmail.fr>
2022-04-26 22:34:33 +0200

    102Disassembler for Python bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-import dis
-
-
 def magic_calculation(a, b, c):
-    if (a < b):
-        return (False)
-    elif (c > b):
-        return (False)
-    else:
-        return (a + b)
-    return (a * b)
-
-
-dis.dis(magic_calculation)
+    if a < b:
+        return c
+    if (c > b):
+        return a + b
+    return a * b - c
