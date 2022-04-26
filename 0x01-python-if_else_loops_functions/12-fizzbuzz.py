f22441e32cb5ce036422db411804f84f0e628fe4
siham <phenissia2009@hotmail.fr>
2022-04-26 22:25:44 +0200

    jeu du fizzbuzz.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end='')
-        elif i % 3 == 0:
-            print("Fizz", end='')
-        elif i % 5 == 0:
-            print("Buzz", end='')
-        else:
-            print(i, end='')
+        mot = i
+        if (i % 3 == 0 and i % 5 == 0):
+            mot = "FizzBuzz"
+        elif (i % 3 == 0):
+            mot = "Fizz"
+        elif (i % 5 == 0):
+            mot = "Buzz"
+        print("{}".format(mot), end=' ')
+        
