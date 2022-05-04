b2e198fc89937a256b89dcdee34d45a0cb0febe3
siham <phenissia2009@hotmail.fr>
2022-05-04 22:00:32 +0200

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+'''
+    Write a function that returns a key with the biggest integer value.
+'''
+
+
 def best_score(a_dictionary):
-    if (a_dictionary is not None):
-        return max(a_dictionary)
-    else:
+
+    bestScore = 0
+    bestScore_name = ""
+
+    if a_dictionary is None:
         return None
+
+    if not a_dictionary:
+        return None
+
+    for key in a_dictionary:
+        if a_dictionary[key] > bestScore:
+            bestScore = a_dictionary[key]
+            bestScore_name = key
+
+    return bestScore_name
