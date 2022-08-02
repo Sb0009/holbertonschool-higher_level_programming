8beb8c9967a2789086971fa9be93def189b76861
siham <phenissia2009@hotmail.fr>
2022-08-02 10:51:39 +0200

    method charPrint(c)

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+   class Square that defines a square
+   and inherits from Square of 5-square.js
+*/
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  /*
+    method charPrint(c) that prints
+    the rectangle using the character c
+  */
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
