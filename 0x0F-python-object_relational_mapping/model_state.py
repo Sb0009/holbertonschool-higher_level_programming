f4af103fd8d128edc61cb695183c8e5ff89a3dbb
siham <phenissia2009@hotmail.fr>
2022-08-07 21:19:48 +0200

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+"""Module contains the class definition of a State and an Instance
+ Base = declarative_base()"""
 
-from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """The class States"""
+    __tablename__ = 'states'
+    id = Column(Integer,  nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
