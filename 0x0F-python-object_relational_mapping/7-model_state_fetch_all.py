91c795b1cc13dc02c3e8f1a192a279265dd43e6a
siham <phenissia2009@hotmail.fr>
2022-08-07 21:23:27 +0200

    FETCHALL

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to create a class
+"""
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sqlalchemy
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    data = session.query(State).all()
+
+    for item in data:
+        print(item)
