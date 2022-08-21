f59d52081308fb1577a55a0afc043a769058b77f
siham Badyine <sihambadyine@gmail.com>
2022-08-21 17:58:14 +0200

    exercice

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@
  the URL and displays the value of the X-Request-Id 
 variable found in the header of the response.'''
 
+
 if __name__ == '__main__':
     from urllib.request import urlopen
     from sys import argv
