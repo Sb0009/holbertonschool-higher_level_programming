7d55032b609c268c695a0d9438d71115b5cc0e74
siham Badyine <sihambadyine@gmail.com>
2022-08-21 18:00:58 +0200

    pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
  the URL and displays the value of the X-Request-Id 
 variable found in the header of the response.'''
 
-
 if __name__ == '__main__':
+    
     from urllib.request import urlopen
     from sys import argv
     with urlopen (argv[1]) as response :
