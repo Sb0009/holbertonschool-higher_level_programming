b30db060e15ef7e6245b3d37655ccc46dd539568
siham Badyine <sihambadyine@gmail.com>
2022-08-22 09:52:11 +0200

    requirement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ variable found in the header of the response.'''
 from urllib.request import urlopen
 from sys import argv
 
+
 if __name__ == "__main__":
     with urlopen(argv[1]) as response:
         print(response.getheader('X-Request-Id'))
